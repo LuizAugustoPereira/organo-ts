ffbc9ed9ec342b28903caebe68eb933a5975ddf5
unknown <gutinholadeira@hotmail.com>
2023-02-25 16:38:57 -0300

    feat: migrates the TextField component to typescript

---
+++ src/componentes/CampoTexto/index.tsx
@@ -0,0 +1,33 @@
+import './CampoTexto.css'
+
+interface CampoTextoProps {
+    obrigatorio: boolean
+    label: string
+    placeholder: string
+    valor: string
+    aoAlterado: (valor: string) => void
+}
+
+const CampoTexto = ({ obrigatorio, label, placeholder, valor, aoAlterado }: CampoTextoProps) => {
+
+    const placeholderModificada = `${placeholder}...` 
+
+    const aoDigitado = (evento: React.ChangeEvent<HTMLInputElement>) => {
+        aoAlterado(evento.target.value)
+    }
+
+    return (
+        <div className="campo-texto">
+            <label>
+                {label}
+            </label>
+            <input
+                value={valor}
+                onChange={aoDigitado}
+                required={obrigatorio}
+                placeholder={placeholderModificada}/>
+        </div>
+    )
+}
+
+export default CampoTexto
